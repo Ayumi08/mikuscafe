d857ae89106ee88e129b409269a26684b20c88a0
Ayumi08 <ayuminakayama265@gmail.com>
2025-05-15 08:51:54 -0700

    initial reaction messages completed

--- extensions/events.py
+++ extensions/events.py
@@ -23,3 +23,16 @@ class MessageEvents(interactions.Extension):
             )
             embed.set_image(url="https://images-ext-1.discordapp.net/external/_8uAGYmcG11pgX5gWLCwCnUnI6abZiOvw-oS5TfseAM/https/retrorender.online/UbKYB1j90.gif?width=400&height=157")
             await event.message.channel.send(embed=embed)
+        # Reaction to mentions event
+        elif event.message.mention_users:
+            mentioned_users = [user async for user in event.message.mention_users]
+            if any(mention.id == 705137748884848691 for mention in mentioned_users):
+                await event.message.add_reaction("<:firefly:1372594981340053616>")
+            elif any(mention.id == 332262693626970112 for mention in mentioned_users):
+                await event.message.add_reaction("<:batman:1372598000492351529>")
+            elif any(mention.id == 148697141076951041 for mention in mentioned_users):
+                await event.message.add_reaction("<:retrorender:1372598417506828470>")
+            elif any(mention.id == 792064920413274142 for mention in mentioned_users):
+                await event.message.add_reaction("<:fucku:1372598033132290129>")
+            elif any(mention.id == 929368321365798932 for mention in mentioned_users):
+                await event.message.add_reaction("<:lenshock:1372601063932428388>")
